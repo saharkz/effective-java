8e98929c42c910badf0db942e508a56df4b7c909
saharkz <karimzadeh.sa@gmail.com>
2020-10-09 23:21:09 +0330

    add item 6 test

--- src/main/java/TestMain.java
+++
@@ -1,7 +0,0 @@
-public class TestMain {
-
-    public static void main(String[] args) {
-        String s1 = new String("Bikini");
-        String s2 = "Bikini";
-    }
-}
--- src/test/java/example/ExampleTest.java
+++ src/test/java/example/ExampleTest.java
@@ -1,15 +1,13 @@
-//package example;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//
-//public class ExampleTest {
-//
-//    @Disabled
-//    @Test
-//    public void methodTest(){
-//
-//        Assertions.assertFalse(true);
-//    }
-//}
+package example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void methodTest() {
+
+        Assertions.assertFalse(false);
+    }
+}
